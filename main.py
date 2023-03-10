8a91a0a19267ef2e26635d56eb9523a518ba11e7
SoUmNerd <102987427+SoUmNerd@users.noreply.github.com>
2023-03-10 19:06:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,22 +16,22 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 client = nextcord.Client(intents=intents)
 
 
-@bot.event
+@bot.event 
 async def on_ready():
     print("Bot Online!")
-    print(f"Logado usando o token {Fore.GREEN}{token}{Style.RESET_ALL}!")
+    print(f"Logged in using token {Fore.GREEN}{token}{Style.RESET_ALL}!")
 
     
 
 @bot.slash_command()
-async def convidar(interaction: nextcord.Interaction):  #Comando que cria um Embed de convite para o servidor
+async def convidar(interaction: nextcord.Interaction):  #Create a embed 
     """Convida o bot para um servidor!"""
-    embed=nextcord.Embed(title="Convidar", url="https://link_do_convite_do_bot.com") #Troca isso
+    embed=nextcord.Embed(title="Invite", url="https://invite_link_bot.com") #Change it
     embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/755769311775162468/1078103302404853860/Apenas_um_nerd_make_a_armadillo_in_this_style_2dbc8642-c046-49c5-9d3d-22201b134b13.png")
-    embed.add_field(name="Clique para adicionar o bot ao servidor!", value="", inline=False)
+    embed.add_field(name="Click here to invite the bot to your server!", value="", inline=False)
     await interaction.send(embed=embed)
 
 
-bot.add_cog(ChatGPT(bot))
+bot.add_cog(ChatGPT(bot)) #Import the gpt commands
 
 bot.run(token)
